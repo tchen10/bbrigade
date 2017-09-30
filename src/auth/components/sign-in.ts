fba6b349a73bb9144bdd6546d167a04dddc4b4e7
Tiffany Chen <tiffanychen10@gmail.com>
2017-09-30 16:50:50 -0400

    Remove anonymous login.

--- src/auth/components/sign-in.ts
+++ src/auth/components/sign-in.ts
@@ -10,9 +10,7 @@ import {AuthService} from '../services/auth-service';
     template: `
         <div class="row sign-in">
             <h1 class="sign-in__heading">Sign in</h1>
-            <button class="sign-in__button" (click)="signInAnonymously()" type="button">Anonymously</button>
             <button class="sign-in__button" (click)="signInWithGoogle()" type="button">Google</button>
-            <button class="sign-in__button" (click)="signInWithFacebook()" type="button">Facebook</button>
         </div>
     `
 })
