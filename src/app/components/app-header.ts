61abb3fe0c758eb9b9f92630e4ae2f15d2ef01d2
Tiffany Chen <tiffanychen10@gmail.com>
2017-09-30 16:58:25 -0400

    Make collasible navbar for mobile

--- src/app/components/app-header.ts
+++ src/app/components/app-header.ts
@@ -5,8 +5,17 @@ import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@
     selector: 'app-header',
     template: `
         <nav class="navbar navbar-default">
-            <div class="container">
-                <a href="/orders" class="navbar-brand">Bibille Brigade</a>
+            <div class="container-fluid">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a href="/orders" class="navbar-brand">Bibille Brigade</a>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li><a href="/meals">Lunch</a></li>
                     <li><a href="/orders">Orders</a></li>
@@ -14,6 +23,7 @@ import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@
                 <ul class="nav navbar-nav navbar-right">
                     <li *ngIf="authenticated"><a (click)="signOut.emit()" href="#">Sign out</a></li>
                 </ul>
+                </div>
             </div>
         </nav>
     `
